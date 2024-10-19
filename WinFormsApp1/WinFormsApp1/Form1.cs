4428d2eebed406d939a8cb76fd02dcd5a3381055
Mariya245 <75563190+Mariya245@users.noreply.github.com>
2024-10-19 16:31:18 +0300

    first

--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -15,6 +17,26 @@ namespace WinFormsApp1
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button8_Click(object sender, EventArgs e)//mc
+        {
+            memory = 0;
+        }
+
+        private void button7_Click(object sender, EventArgs e)//mr
+        {
+            textBox1.Text = memory.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)//m+
+        {
+            memory += Int32.Parse(textBox1.Text);
+        }
+
+        private void button5_Click(object sender, EventArgs e)//m-
+        {
+            memory -= Int32.Parse(textBox1.Text);
         }
     }
 }
